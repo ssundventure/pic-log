ee8d30eed802aa30ae9e66e5d8dce2399b39dc76
ssundventure <ssundventure@gmail.com>
2025-08-10 15:29:41 +0900

    폴더 구조 만들기

---
+++ src/components/PhotoCard.tsx
@@ -0,0 +1,6 @@
+function PhotoCard(){
+
+    return  <div></div>
+}
+
+export default PhotoCard;   
---
+++ src/components/PhotoList.tsx
@@ -0,0 +1,11 @@
+
+function PhotoList() {
+  return (
+    <div>
+      <h1>Photo List</h1>
+      {/* Photo cards will be rendered here */}
+    </div>
+  );
+}
+
+export default PhotoList;
