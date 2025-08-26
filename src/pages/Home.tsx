e5652c15f7367aacb1b1be55fe2f0d9f6de71123
ssundventure <ssundventure@gmail.com>
2025-08-27 00:51:23 +0900

    home 문구추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,12 @@
 function Home() {
-  return <div>home page</div>;
+  return (
+    <div>
+      <p># PicLog </p>
+      <p>Capture. Write. Remember. </p>
+      <p>나의 하루를 사진과 글로 기록한다.</p>
+      <p>사진 위에 글을 쓰고, 추억을 저장하세요.</p>
+    </div>
+  );
 }
 
 export default Home;
