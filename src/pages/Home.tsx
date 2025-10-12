47f45bfdfbc012e89f72cbacdea58c948e965860
ssundventure <ssundventure@gmail.com>
2025-10-12 21:06:36 +0900

    feat: localstorage의 이미지를 home 화면에 보여주는 기능 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
+
 function Home() {
+  const [latestImg, setlatestImg] = useState<string | null>(null);
+
+  useEffect(() => {
+    const newImg = localStorage.getItem("latestImage");
+    console.log(newImg)
+    if (newImg) {
+      setlatestImg(newImg);
+    }
+  }, []);
+
   return (
-    <div>
-      <p># PicLog </p>
-      <p>Capture. Write. Remember. </p>
-      <p>나의 하루를 사진과 글로 기록한다.</p>
-      <p>사진 위에 글을 쓰고, 추억을 저장하세요.</p>
-    </div>
+    <>
+      {latestImg ? (
+        <img src={latestImg} alt="" />
+      ) : (
+        <div>
+          <p># PicLog </p>
+          <p>Capture. Write. Remember. </p>
+          <p>나의 하루를 사진과 글로 기록한다.</p>
+          <p>사진 위에 글을 쓰고, 추억을 저장하세요.</p>
+        </div>
+      )}
+    </>
   );
 }
 
